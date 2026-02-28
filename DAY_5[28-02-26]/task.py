2480f8ce9511c9e1a7b1e41046e24817bb5644ca
sarikadev30 <sarika.ss.net@gmail.com>
2026-02-28 23:40:05 +0530

    added day 5

---
+++ DAY_5[28-02-26]/task.py
@@ -0,0 +1,90 @@
+# Mathematical Operators
+# d = 3
+# e = 2
+# f = 4
+# sum = (d * 2) + (e * 2) + (f * 2)
+# print(sum)
+# ........................................................
+# String Concatenation
+
+# a = 34
+# b = 2
+# print(a + b)
+
+# p = "Hi.."
+# q = "Sam"
+# print(p + q)               # String Concatenation
+
+# .........................................................
+# Comparison Operators
+
+# a = 5
+# b = 4
+# print(a > b)
+# print(a >= b)
+# print(a < b)
+# print(a <= b)
+# print(a == b)
+# print(a != b)
+
+# .......................................................
+# Problems:
+
+# ram = "ram"
+# print(5 == 5)  # T
+# print("dam" == "dam")  # T
+# print(5 == "5")  # F
+# print(ram == "ram")  # T
+# print(5 == "ram")  # F
+# print(5 != 5)
+# print(6 == "6")
+# print(6 != "6")
+# print(6 != 7)
+# ........................................................
+# Problem 2:
+# age_of_ram = 25; age_of_mohan = 30;
+# Check whether the Ram age is greater than Mohan or not.
+
+# age_of_ram = 25
+# age_of_mohan = 30
+# print()
+# .......................................................
+# Problem 3:
+# sunil_marks = 36; passing_marks = 35;
+# Check whether Sunil is passed or not, where the passing mark is 35
+sunil_marks = 35
+passing_marks = 33
+# .......................................................
+# Problem 4:
+# sunil_marks = 34; passing_marks = 35;
+# Check whether Sunil failed or not, where the passing mark is 35
+# sunil_marks = 34
+# passing_marks = 35
+# ..........................................................
+# Problem 5:
+# Those customers will be eligible for the amazon discount
+# whose spending is equal to or above 4000.
+# Check whether a customer Sam is eligible for a discount or not
+# Sam_Spendings = 4500
+
+# ..........................................................
+
+
+fan = 5
+a = "fan"
+b = fan
+print(a == b)  # F    "fan"  fan  =>  "fan"== 5
+print(a != b)  # T     "fan" != 5
+
+# ..........................................................
+a = 5
+b = 6
+c = "6"
+d = -2
+e = "m"
+print(b == c)
+print(c == e)
+print(a >= c)
+print(d <= a)
+print(b != c)
+print(3.0 == 3)
