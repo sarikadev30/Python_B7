37531af110e04107904934c3ac7a9bdb2e1d5473
sarikadev30 <sarika.ss.net@gmail.com>
2026-03-08 22:36:32 +0530

    added day 7

---
+++ DAY_7[08-03-26]/task.py
@@ -0,0 +1,122 @@
+# Operators
+# Mathematical
+# Comparison
+# Logical
+# Assignment
+# Membership Operators
+# ...................................................................
+# Assignment Operaters
+# x = 8
+
+# +=   Add and assign
+# x += 2  # x = x + 2
+# print(x)  # 10
+
+# -=   Subtract and assign
+# x -= 3  # x = x - 3
+# print(x)  # 7
+
+# *= Multiply and assign
+# x *= 2  # x = x * 2
+# print(x)  # 14
+
+# /= Divide and assign
+# x /= 2  # x = x / 2
+# print(x)  # 7.0
+
+# %= Modulo and assign
+# x %= 2  # x=x%2
+# print(x)
+
+# **= exponentiation and assign
+# y = 3
+# y **= 2  # y= y**2
+# print(y)
+
+# "//=" floor division and assign
+# z = 5
+# z //= 2  # z = z // 2
+# print(z)
+
+# ............................................................................
+# Problems
+# x = 8
+# x *= 2
+# x -= 4
+# print(x)
+# ..........................................
+# a = 5
+# b = 3
+# a %= b
+# print(a)
+# ..........................................
+# x = 101
+# x -= 25
+# x //= 5
+# print(x)
+# ..........................................
+# b = 10
+# b %= 4
+# b *= 2
+# print(b)
+# ...........................................................................
+# Membership Operator
+
+# in
+# val = "hello"
+# val2 = "wor"
+# txt = "hello world !...."
+
+# print(val in txt)
+# print(val2 in txt)
+
+# # not in
+# print(val not in txt)
+
+# x = "How are you?"
+# txt = "Are"
+# print(txt in x)
+
+# xy = [23, 34, 45, 12, 34, 56]  # list
+# val = 34
+# print(val in xy)
+# print(22 not in xy)
+
+# greeting = "good morning"
+# print("good" in greeting)
+# print("Good" in greeting)
+# print("night" not in greeting)
+
+# ..........................................................................
+# Identity Operators
+
+# # -5 to 256
+# a = 12
+# b = 12
+# print(a is b)
+
+# x = 300
+# y = 300
+# print(x is y)  # True
+
+
+#
+# s1 = "hello"
+# s2 = "hello"
+# print(s1 is s2)
+# ...........................................................
+
+# Conditional Statements
+# x = 45
+
+# if
+
+# print("Start")
+# if x > 0:
+#     print("Positive")
+
+# print("End")
+
+# num = 46
+# if num % 5 == 0:
+#     print(f"Yes, {num} is a multiple of 5")
